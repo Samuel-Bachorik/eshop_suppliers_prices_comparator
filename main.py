71efc0f5a4299a54718c8ce87c1bb0ac2ee9c530
Samuel Bachorík <61843287+Samuel-Bachorik@users.noreply.github.com>
2022-01-30 19:16:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from google_sheets import get_sheet
-from get_prices import get_price_alza, get_price_heureka, get_price_gigastore, remove_trash
+from get_prices import get_price_alza, get_price_heureka, get_price_gigastore, get_price_axdata,get_price_tvojpc\
+    ,get_price_dmcomp,get_price_datacomp,get_price_prva,get_price_zdomu,remove_trash
 from concurrent.futures import ProcessPoolExecutor
 import concurrent.futures
+import os
+import time
+
 ID = '114oIOb8Ml45ET3aziBiaBlOl0TpbtLMT8-fkj6xGTTM'
 JSON_KLUC = "eshop-sheet-c38c9c8a59ed.json"
 sheets = get_sheet(ID, JSON_KLUC)
-import os
 
-import time
 def _run_workers(processes_count):
     start_time = time.time()
     num = processes_count
@@ -23,15 +25,13 @@ def _run_workers(processes_count):
         stop_start_ids[z][0] = stop_start_ids[z-1][1]
         stop_start_ids[z][1] = stop_start_ids[z][0] + num_for_process
 
-    print(stop_start_ids,"IDS")
-
-    
+    #print(stop_start_ids,"IDS")
+    print("Started...")
     with ProcessPoolExecutor(max_workers=num) as executor:
         results = [None] * num
         for x in range(num):
             results[x] = executor.submit(process, sheets, stop_start_ids[x])
 
-        #counter = 0
         errors = []
         for f in concurrent.futures.as_completed(results):
             x = f.result()
@@ -41,50 +41,57 @@ def _run_workers(processes_count):
             for x in i:
                 print(x)
 
-        print("--- %s seconds ---" % (time.time() - start_time))
-
-          #  counter += 1
-
-    #return result_x, result_y
+        print("This task took %s seconds" % (time.time() - start_time))
 
 
 def process(sheet, start_stop):
     errors = []
+    supplier_database = ["gigastore", "datacomp", "axdata", "tvojpc", "zdomu", "prva", "dmcomp"]
+    supplier_database_f = [get_price_gigastore, get_price_datacomp, get_price_axdata, get_price_tvojpc,
+                         get_price_zdomu, get_price_prva, get_price_dmcomp]
 
     for i in range(start_stop[0],start_stop[1]):
 
-        price = False
-        product_miss = False
-        lowest_price = False
+        margin_in_percent = 2
 
-        price_dodavatel = get_price_gigastore(sheet[i][1])
+        supplier_price_err = False
+        product_margin_err = False
+        lowest_price_err = False
+
+        for j in range(len(supplier_database)):
+            if supplier_database[j] in sheet[i][1]:
+                price_supplier = supplier_database_f[j](sheet[i][1])
+
         price_heureka = get_price_heureka(sheet[i][2])
         price_alza = get_price_alza(sheet[i][0])
 
-        if price_heureka  == False or price_dodavatel == False:
-            product_miss = True
-            errors.append("Produkt "+  str(i+2) + " zmizol")
+        if price_supplier == "UNAVAILABLE":
+            errors.append("Product " + str(i + 2) + " is unavailable")
 
             continue
 
-        price_dodavatel = remove_trash(price_dodavatel)
+        if price_heureka  == False or price_supplier == False:
+            errors.append("Product "+  str(i+2) + " dissapeared from supplier database")
+
+            continue
+
+        price_supplier = remove_trash(price_supplier)
         price_heureka = remove_trash(price_heureka)
         price_alza = remove_trash(price_alza)
 
 
-        """if i == 10:
-            print(price_dodavatel)
-            print(price_heureka)
-            print(price_alza)"""
-
-        if price_alza < price_dodavatel:
-            price = True
+        if price_alza < price_supplier:
+            supplier_price_err = True
 
         if price_heureka < price_alza:
-            lowest_price = True
+            lowest_price_err = True
 
-        if price or lowest_price:
-            errors.append("Na produkte "+ str(i+2) + " Cena dodavatela vyššia - " + str(price) +"   |||   Existuje nizsia cena - " + str(lowest_price))
+        if (abs(price_alza - price_supplier) / price_supplier) * 100.0 < margin_in_percent:
+            product_margin_err = True
+
+        if supplier_price_err or lowest_price_err or product_margin_err:
+            errors.append("At product "+ str(i+2) + " Price of supplier is higher - " + str(supplier_price_err) +
+                          "   |||   Lower price exist - " + str(lowest_price_err)+"   |||   Low margin - " + str(product_margin_err))
 
     return errors
 
@@ -92,4 +99,4 @@ def process(sheet, start_stop):
 if __name__ == '__main__':
     os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
-    _run_workers(processes_count= 20)
+    _run_workers(processes_count= 10)
